d314cfc27ae216aa0965ae23387220924e3fadc5
AngelLiang <pl01665077@163.com>
2024-07-26 11:27:35 +0800

    feat: 添加大模型功能

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import os
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
+from services import ChatClient
+from dotenv import load_dotenv
+load_dotenv()
 
+SPARK_APPID = os.getenv('SPARK_APPID', None)
+SPARK_APISECRET = os.getenv('SPARK_APISECRET', None)
+SPARK_APIKEY = os.getenv('SPARK_APIKEY', None)
 
 app = FastAPI()
 
@@ -14,6 +23,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
+class GenerateIn(BaseModel):
+    prompt: str
+
+
+@app.post('/api/generate')
+async def generate(payload: GenerateIn):
+    prompt = payload.prompt
+    stream = ChatClient(SPARK_APIKEY, SPARK_APISECRET).generate(
+        prompt, stream=True)
+    return StreamingResponse(stream)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000,  reload=True, log_level="info")
---
+++ services.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+
+class ChatClient:
+    def __init__(self, apikey: str, secret: str) -> None:
+        self.apikey = apikey
+        self.secret = secret
+        self.client = OpenAI(
+            # 控制台获取key和secret拼接，假使APIKey是key123456，APISecret是secret123456
+            api_key=f"{apikey}:{secret}",
+            base_url='https://spark-api-open.xf-yun.com/v1'  # 指向讯飞星火的请求地址
+        )
+
+    def generate(self, content: str, stream=True) -> str:
+        completion = self.client.chat.completions.create(
+            model='general',  # 指定请求的版本
+            messages=[
+                {
+                    "role": "user",
+                    "content": f'你是一位小红书生成器，请针对用户的关键词编写一篇小红书文案，需要符合小红书风格，需要包括标题、正文和标签三部分。用户关键词是：{content}'
+                }
+            ],
+            stream=stream
+        )
+        for chunk in completion:
+            data = str(chunk.choices[0].delta.content)
+            data = data.replace('\n', '<br>')
+            yield data
