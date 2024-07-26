1f69941f06085b388357f576f2fca492bd032805
AngelLiang <pl01665077@163.com>
2024-07-26 10:12:45 +0800

    feat: 添加主页

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get('/')
-def index():
-    return 'hello'
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
